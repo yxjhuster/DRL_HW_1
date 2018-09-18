10b820bd876f338110d3327806c0631aee4291c2
yxjhuster <yxjhuster@gmail.com>
2018-09-18 17:11:42 -0400

    Finish the write-up

--- agent_simulator.py
+++ agent_simulator.py
@@ -44,27 +44,31 @@ def stochastic_run(env, gamma, tol = 1e-3):
 # print("computed value: " + str(total_reward))
 # print("simulated value: " + str(value_func[initial_state]))
 # print('\n')
-#
+
 env = gym.make('Stochastic-4x4-FrozenLake-v0')
 gamma = 0.9
+max = 1e3
 initial_state = env.reset()
 policy, iteration, value_func= rl.value_iteration_sync(env, gamma)
 reward = stochastic_run(env, gamma)
 tol = 1e-10
+policy_tol, iteration_tol, value_func_tol= rl.value_iteration_sync(env, gamma,max,tol)
 reward_tol = stochastic_run(env, gamma, tol)
-print("computed value: " + str(reward))
-print("computed value (tol = 1e-10): " + str(reward_tol))
+print("computed value: " + str(reward_tol))
+print("computed value (tol = 1e-10): " + str(value_func_tol[initial_state]))
 print("simulated value: " + str(value_func[initial_state]))
 print('\n')
 
-# env = gym.make('Stochastic-8x8-FrozenLake-v0')
-# gamma = 0.9
-# initial_state = env.reset()
-# policy, iteration, value_func= rl.value_iteration_sync(env, gamma)
-# # reward = stochastic_run(env, gamma)
-# tol = 1e-10
-# reward_tol = stochastic_run(env, gamma, tol)
-# # print("computed value: " + str(reward))
-# print("computed value (tol = 1e-10): " + str(reward_tol))
-# print("simulated value: " + str(value_func[initial_state]))
-# print('\n')
+env = gym.make('Stochastic-8x8-FrozenLake-v0')
+gamma = 0.9
+max = 1e3
+initial_state = env.reset()
+policy, iteration, value_func= rl.value_iteration_sync(env, gamma)
+reward = stochastic_run(env, gamma)
+tol = 1e-10
+policy_tol, iteration_tol, value_func_tol= rl.value_iteration_sync(env, gamma,max,tol)
+reward_tol = stochastic_run(env, gamma, tol)
+print("computed value: " + str(reward_tol))
+print("computed value (tol = 1e-10): " + str(value_func_tol[initial_state]))
+print("simulated value: " + str(value_func[initial_state]))
+print('\n')
